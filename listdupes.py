dd8973a8096c2aea2d49168e5f63b3cd6941b18d
Chris-Dobbins <107791888+Chris-Dobbins@users.noreply.github.com>
2022-06-19 12:23:12 -0700

    Add an optional progress counter

--- listdupes.py
+++ listdupes.py
@@ -7,7 +7,7 @@ Writes its output to listdupes_output.csv in the user's home folder.
 """
 
 # Module Attributes
-__version__ = "3.4.0"
+__version__ = "4.0.0"
 __author__ = "Chris Dobbins"
 __license__ = "BSD-2-Clause"
 
@@ -16,9 +16,99 @@ __license__ = "BSD-2-Clause"
 import argparse
 import collections
 import csv
+import glob
 import pathlib
 import sys
 from zlib import crc32 as checksummer
+
+
+# Classes
+class Cursor:
+    hide_cursor = "\x1b[?25l"
+    show_cursor = "\x1b[?25h"
+
+    def hide_cursor_from_user(self):
+        print(self.hide_cursor, file=self.output, end="")
+
+    def set_cursor_column_to(self, column_number):
+        set_cursor_column_esc_code = f"\x1b[{column_number}G"
+        print(set_cursor_column_esc_code, file=self.output, end="")
+
+
+class ProgressCounter(Cursor):
+    """Methods for printing the state of an iteration to a terminal."""
+
+    def __init__(
+        self,
+        total_to_be_counted,
+        text_before_counter="",
+        text_after_counter="",
+        output=sys.stderr,
+    ):
+        self.total_to_be_counted = total_to_be_counted
+        self.text_before_counter = text_before_counter
+        self.text_after_counter = text_after_counter
+        self.output = output
+
+        # TODO: Address length_of_total only being foolproof if total comes from len().
+        self.length_of_total = len(str(total_to_be_counted))
+        self.length_of_text_before_counter = len(self.text_before_counter)
+        self.length_of_text_after_counter = len(self.text_after_counter)
+        self.start_of_counter = self.length_of_text_before_counter + 1
+        self.after_counter = (
+            self.length_of_text_before_counter + self.length_of_total + 1
+        )
+        self.after_text = (
+            self.length_of_text_before_counter
+            + self.length_of_total
+            + self.length_of_text_after_counter
+        )
+        self.move_to_start_of_counter = f"\x1b[{self.start_of_counter}G"
+
+    def print_text_for_counter(self):
+        """Convenience method which handles the most typical setup."""
+        self.hide_cursor_from_user()
+        self.print_text_before_counter()
+        self.print_text_after_counter(self.after_counter)
+        self.set_cursor_column_to(self.start_of_counter)
+
+    def print_counter(self, current_index):
+        """Counter designed to be inserted in loops."""
+        counter_value = str(current_index + 1).rjust(self.length_of_total, "0")
+        print(
+            counter_value,
+            self.move_to_start_of_counter,
+            file=self.output,
+            flush=True,
+            sep="",
+            end="",
+        )
+
+    def print_counter_and_end_count(self, current_index):
+        """Convenience method that might be a touch slower."""
+        self.print_counter(current_index)
+        if (current_index + 1) == self.total_to_be_counted:
+            self.end_count()
+
+    def print_text_before_counter(self):
+        """Example text: 'Checking file '"""
+        print(self.text_before_counter, file=self.output, end="")
+
+    def print_text_after_counter(self, starting_column_for_cursor):
+        """Example text: ' of {}.'"""
+        formatted_text = self.text_after_counter.format(self.total_to_be_counted)
+        self.set_cursor_column_to(starting_column_for_cursor)
+        print(
+            formatted_text,
+            sep="",
+            file=self.output,
+            end="",
+        )
+
+    def end_count(self, append_newline=True):
+        end_arg = "\n" if append_newline else ""
+        self.set_cursor_column_to(self.after_text)
+        print(self.show_cursor, file=self.output, end=end_arg)
 
 
 # Functions
@@ -58,6 +148,28 @@ def checksum_paths(collection_of_paths):
     return paths_and_checksums
 
 
+def checksum_paths_and_show_progress(collection_of_paths):
+    checksum_progress = ProgressCounter(
+        len(collection_of_paths),
+        text_before_counter="Checking file ",
+        text_after_counter=" of {}.",
+    )
+    paths_and_checksums = []
+    try:
+        checksum_progress.print_text_for_counter()
+        for index, file_path in enumerate(collection_of_paths):
+            try:
+                with open(file_path, mode="rb") as file:
+                    checksum = checksummer(file.read())
+            except IsADirectoryError:
+                continue  # Skip directories.
+            paths_and_checksums.append((file_path, checksum))
+            checksum_progress.print_counter(index)
+    finally:
+        checksum_progress.end_count()
+    return paths_and_checksums
+
+
 def find_dupes(paths_and_checksums):
     dupes = collections.defaultdict(list)
     for index, element in enumerate(paths_and_checksums):
@@ -67,6 +179,28 @@ def find_dupes(paths_and_checksums):
             checksums_are_equal = checksum_being_searched == current_checksum
             if checksums_are_equal and path_not_in_dict(path_being_searched, dupes):
                 dupes[path_being_searched].append(current_path)
+    return dupes
+
+
+def find_dupes_and_show_progress(paths_and_checksums):
+    comparisons_progress = ProgressCounter(
+        len(paths_and_checksums),
+        text_before_counter="Comparing file ",
+        text_after_counter=" of {}.",
+    )
+    dupes = collections.defaultdict(list)
+    try:
+        comparisons_progress.print_text_for_counter()
+        for index, element in enumerate(paths_and_checksums):
+            path_being_searched, checksum_being_searched = element
+            comparisons_progress.print_counter(index)
+
+            for current_path, current_checksum in paths_and_checksums[index + 1 :]:
+                checksums_are_equal = checksum_being_searched == current_checksum
+                if checksums_are_equal and path_not_in_dict(path_being_searched, dupes):
+                    dupes[path_being_searched].append(current_path)
+    finally:
+        comparisons_progress.end_count()
     return dupes
 
 
@@ -114,6 +248,12 @@ def main():
         "starting_folder",
         help="accepts a single path from the terminal",
     )
+    parser.add_argument(
+        "-p",
+        "--progress",
+        action="store_true",
+        help="print a progress counter to stderr, can slow things down",
+    )
     args = parser.parse_args()
 
     # Determine the output's eventual file path.
@@ -127,14 +267,24 @@ def main():
     # Gather all files except for those starting with a period.
     unexpanded_starting_path = pathlib.Path(args.starting_folder)
     starting_path = unexpanded_starting_path.expanduser()
-    sub_paths = starting_path.glob("**/[!.]*")
+    if args.progress:
+        glob_module_arg = str(starting_path) + "/**/[!.]*"
+        sub_paths = glob.glob(glob_module_arg, recursive=True)
+    else:
+        sub_paths = starting_path.glob("**/[!.]*")
 
     # Checksum the files.
-    files_and_checksums = checksum_paths(sub_paths)
+    if args.progress:
+        files_and_checksums = checksum_paths_and_show_progress(sub_paths)
+    else:
+        files_and_checksums = checksum_paths(sub_paths)
 
     # Compare the checksums and make a dictionary of duplicate files.
     files_and_checksums.sort()
-    dupes = find_dupes(files_and_checksums)
+    if args.progress:
+        dupes = find_dupes_and_show_progress(files_and_checksums)
+    else:
+        dupes = find_dupes(files_and_checksums)
 
     # Format the duplicate paths as a CSV and write it to a file.
     try:
