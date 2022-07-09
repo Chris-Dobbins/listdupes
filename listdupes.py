cafa56f491c26b3125da13ce83d6b488e0e82351
Chris Dobbins <107791888+Chris-Dobbins@users.noreply.github.com>
2022-07-09 08:23:33 +0000

    Fix typo

--- listdupes.py
+++ listdupes.py
@@ -7,7 +7,7 @@ Writes its output to listdupes_output.csv in the user's home folder.
 """
 
 # Module Attributes
-__version__ = "Neutral"
+__version__ = "Internal"
 __author__ = "Chris Dobbins"
 __license__ = "BSD-2-Clause"
 
