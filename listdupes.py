50a1ad41492f5b836ae44d877d6e7c58ad3e4fdd
Chris Dobbins <107791888+Chris-Dobbins@users.noreply.github.com>
2022-07-16 11:40:40 +0000

    Fix buggy kwarg usage in write_dupes_to_csv

--- listdupes.py
+++ listdupes.py
@@ -324,7 +324,10 @@ def write_dupes_to_csv(
         **kwargs: Passed to the open function.
     """
 
-    with open(output_file, mode=mode, **kwargs) as csv_file:
+    # with open(output_file, mode=mode, **kwargs) as csv_file:
+    with open(
+        output_file, encoding=encoding, mode=mode, errors=errors, **kwargs
+    ) as csv_file:
         writer = csv.writer(csv_file)
         if labels:
             writer.writerow(labels)
