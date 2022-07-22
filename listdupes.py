21e49c5d2e4911574c1d18590c3dabf315eb9114
Chris Dobbins <107791888+Chris-Dobbins@users.noreply.github.com>
2022-07-22 03:21:30 +0000

    Bump __version__

--- listdupes.py
+++ listdupes.py
@@ -7,7 +7,7 @@ Writes its output to listdupes_output.csv in the user's home folder.
 """
 
 # Module Attributes
-__version__ = "6.0.0-alpha.2"
+__version__ = "6.0.0-alpha.3"
 __author__ = "Chris Dobbins"
 __license__ = "BSD-2-Clause"
 
