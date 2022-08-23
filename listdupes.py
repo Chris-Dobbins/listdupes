bde140b151fef00e9c12c2fb437d8f9853a9250c
Chris Dobbins <107791888+Chris-Dobbins@users.noreply.github.com>
2022-08-23 19:57:11 +0000

    Fix bug preventing os_errors retrival from cache

--- listdupes.py
+++ listdupes.py
@@ -665,7 +665,8 @@ def _read_cache_from(file, **kwargs):
     cached["starting_path_from_archive"] = pathlib.Path(
         cached["starting_path_from_archive"]
     )
-    cached["os_errors"] = {k: set(v) for k, v in cached["os_errors"].items()}
+    for key, value in cached["os_errors"].items():
+        cached["os_errors"][key] = [(x, y) for x, y in value]
     cached["paths_and_sums"] = [
         (pathlib.Path(x), y) for x, y in cached["paths_and_sums"]
     ]
